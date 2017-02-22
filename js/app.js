73f2cb8b284e7ab69fbc16f69d86f44046175c06
cr0wbait <etcothran@gmail.com>
2017-02-22 15:30:37 -0500

    building html and styles

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-import $ from 'jquery'
-import getJSON from 'JSON'
-
 console.log('prepare the ray gun!')
+
+
+let congressTile = `
+<div class='congressTile col-sm-4'>
+<h3>${serverRes.results.name}</h1>
+<h4>${}</h4>
+<ul>
+  <li>email: ${}</li>
+  <li>website: ${}</li>
+  <li>facebook: ${}</li>
+  <li>twitter: ${}</li>
+</ul>
+<h6>
+  ${}
+</h6>
+</div>
+`
