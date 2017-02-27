54d006e018572ddbe6deb7e508531d176256f2ff
cr0wbait <etcothran@gmail.com>
2017-02-27 15:29:56 -0500

    finally got it wired up woo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
-console.log('prepare the ray gun!')
+// console.log($)
+// import $ from 'jquery'
+
+console.log($)
+
+$.getJSON('https://congress.api.sunlightfoundation.com/legislators?callback=?').then((serverRes)=>{
+  console.log(serverRes)
+  let bigHtmlStrng = serverRes.results.map(function(data){
 
 
-let congressTile = `
-<div class='congressTile col-sm-4'>
-<h3>${serverRes.results.name}</h1>
-<h4>${}</h4>
-<ul>
-  <li>email: ${}</li>
-  <li>website: ${}</li>
-  <li>facebook: ${}</li>
-  <li>twitter: ${}</li>
-</ul>
-<h6>
-  ${}
-</h6>
-</div>
-`
+    var htmlStr = `
+    <div class='col-sm-4 congressTile'>
+    <h4>${data.first_name} ${data.last_name}</h4>
+    <h5>${data.title} -- ${data.party}-${data.state_name}</h5>
+    <ul>
+      <li>Email: ${data.oc_email}</li>
+      <li>Website: ${data.website}</li>
+      <li>FaseBuk: ${data.facebook_id}</li>
+      <li>Twitter: ${data.twitter_id}</li>
+    </ul>
+    <h6>Term End: ${data.term_end}</h6>
+    </div>
+    `
+    return htmlStr
+  }).join('')
+
+  let congMap = document.querySelector('.congMap')
+    congMap.innerHTML += bigHtmlStrng
+})
